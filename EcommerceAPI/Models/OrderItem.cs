22d0b45b3a88b124e0abaea263e5665ce0bf3299
Pedro Henrique Gasquez <phlgasquez@gmail.com>
2025-09-13 00:25:25 -0300

    Add OrderItem model

---
+++ EcommerceAPI/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceAPI.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        //FR para Order e Product
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantidade { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecoUnitario { get; set; }
+
+        [NotMapped]
+        public decimal Subtotal => PrecoUnitario * Quantidade;
+    }
+}
