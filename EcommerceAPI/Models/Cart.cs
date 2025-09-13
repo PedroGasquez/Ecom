56d52643af74e54e60e53775a36a205010212ba4
Pedro Henrique Gasquez <phlgasquez@gmail.com>
2025-09-13 00:24:56 -0300

    Add Cart model

---
+++ EcommerceAPI/Models/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceAPI.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        // FR para User
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+
+        [NotMapped]
+        public decimal Total => Items?.Sum(i => i.Preco * i.Quantidade) ?? 0;
+    }
+}
