8ed8d677663069fab411a3c59d1b94397a359cda
Pedro Henrique Gasquez <phlgasquez@gmail.com>
2025-09-13 00:13:05 -0300

    initial commit

---
+++ EcommerceAPI/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Nome { get; set; }
+
+        [StringLength(1000)]
+        public string? Descricao { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Preco { get; set; }
+
+        [Required]
+        public int EstoqueQuantidade { get; set; }
+
+        [StringLength(500)]
+        public string? ImagemUrl { get; set; }
+
+        public bool Ativo { get; set; } = true;
+
+        // Fkey para Category
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ EcommerceAPI/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceAPI.Models
+
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public Cart? Cart { get; set; }
+    }
+}
