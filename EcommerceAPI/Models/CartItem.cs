97a12f6a7e8d893465a20181cb5c66a4e9fa6e88
Pedro Henrique Gasquez <phlgasquez@gmail.com>
2025-09-13 00:21:00 -0300

    Added CartItem model

---
+++ EcommerceAPI/Models/CartItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceAPI.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        // FR para Cart
+        public int CartId { get; set; }
+        public Cart Cart { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantidade { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Preco { get; set; } 
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [NotMapped]
+        public decimal Subtotal => Preco * Quantidade;
+    }
+}
